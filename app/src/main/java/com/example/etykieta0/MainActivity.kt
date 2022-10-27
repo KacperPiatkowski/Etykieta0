349774d59b7a477e281bbb112cc11149e94215c6
Kacper <kacper.piatkowski@sailingbyte.com>
2022-10-28 00:16:45 +0200

    skonczone

--- app/src/main/java/com/example/etykieta0/MainActivity.kt
+++ app/src/main/java/com/example/etykieta0/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.etykieta0
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*findViewById<Button>(R.id.buttonTekst).setOnClickListener {
+            var text1 = findViewById<TextView>(R.id.textViewEtykieta).text.toString();
+            var text2 = findViewById<EditText>(R.id.editTekst).text.toString();
+            findViewById<TextView>(R.id.textViewEtykieta).text = text1 + text2;
+        }
+        findViewById<Button>(R.id.buttonImNaz).setOnClickListener {
+            findViewById<TextView>(R.id.textViewEtykieta).text = "Kacper Piątkowski";
+        }*/
     }
 }
